6167cbf7c638e998721803e2c357132e566b499b
Batyrbek <https://github.com/bakytkereyb/assignment4.git>
2021-04-28 00:45:07 +0500

    added FAQ option, login page

--- web-site/js/faq.js
+++ web-site/js/faq.js
@@ -1,6 +1,8 @@
 function showFAQ(question_id) {
   var answer = "#" + question_id + "-answer";
   $(answer).toggleClass('showed');
+  var text = "#" + question_id + "-text";
+  $(text).toggleClass('changed');
   var icon = "#" + question_id + "-icon";
   $(icon).toggleClass('rotated');
 }
